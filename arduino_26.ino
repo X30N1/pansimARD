c5204605f88ff624d9f993bf4c3ff4cddb49d951
X30N1 <sowapocztowa1@gmail.com>
2025-03-17 08:48:42 +0100

    update pinów + wykonanie poprzedniego pusha

--- arduino_26.ino
+++ arduino_26.ino
@@ -16,11 +16,21 @@ const int sygZl1_3 = 7; // D7 na A-D42
 
 const int sygZ = 8; // D8 na A-D43
 
+/// Światła nr.2 (dalej od syg. kolejowej)
+const int sygC2 = 12; // D12 na A-D38
+
+const int sygZl2_1 = 9; // D9 na A-D52
+const int sygZl2_2 = 10; // D10 na A-D54
+const int sygZl2_3 = 11; // D11 na A-D55
+
+const int sygZ2 = 13; // D13 na A-D56
+
+// Zmienne funkcji
 bool zmiana = false; //Zmienna do zmiany świateł
 int czasZmiany = 1500; //Ile czasu sygnalizacja świetlna będzie czekać pomiędzy zamianą
 
 void setup() {
-  // konfiguracja pinów, skrypt wykonuje się raz na początku:
+  // Konfiguracja pinów, skrypt wykonuje się raz na początku:
 
   // Sygnalizacja Kolejowa
   pinMode(cz1, OUTPUT);
@@ -34,28 +44,88 @@ void setup() {
   pinMode(sygZl1_3, OUTPUT);
   pinMode(sygZ, OUTPUT);
 
-  Serial.print("test boot");
+  // Sygnalizacja drogowa nr.2
+  pinMode(sygC2, OUTPUT);
+  pinMode(sygZl2_1, OUTPUT);
+  pinMode(sygZl2_2, OUTPUT);
+  pinMode(sygZl2_3, OUTPUT);
+  pinMode(sygZ2, OUTPUT);
+
+  // Miejsce na wstawienie funkcji stałych
+  testRGB(255, 0, 0);
 }
 
 void loop() {
   // Kod który wykonuje się cały czas:
-  SygnalizacjaDrogowa();
+  SygnalizacjaKolejowa();
+}
 
+void sygnalizacjaSetup() {
+  // Funkcja do ustawienia sygnalizacji drogowej, należy używać w void setup()
+  digitalWrite(sygC, HIGH);
+  digitalWrite(sygZl1_1, HIGH);
+  digitalWrite(sygZl1_2, HIGH);
+  digitalWrite(sygZl1_3, HIGH);
+  digitalWrite(sygZ, LOW);
+
+  digitalWrite(sygC2, LOW);
+  digitalWrite(sygZl2_1, HIGH);
+  digitalWrite(sygZl2_2, HIGH);
+  digitalWrite(sygZl2_3, HIGH);
+  digitalWrite(sygZ2, HIGH);
 }
 
 void SygnalizacjaDrogowa() {
-  // Symulacja sygnalizacji drogowej przełączanej guzikiem TODO
-  
-  if (digitalRead(guzik) == HIGH) {
+  // Symulacja sygnalizacji drogowej przełączanej guzikiem, należy używać z sygnalizacjaSetup na void setup() 
+  if (digitalRead(guzik) == HIGH && zmiana == false) {
+
     digitalWrite(sygC, LOW);
     digitalWrite(sygZl1_1, LOW);
     digitalWrite(sygZl1_2, LOW);
     digitalWrite(sygZl1_3, HIGH);
-    delay(1000);
+
+    digitalWrite(sygZ2, LOW);
+    digitalWrite(sygZl2_1, LOW);
+    digitalWrite(sygZl2_2, LOW);
+    digitalWrite(sygZl2_3, HIGH);
+
+    delay(czasZmiany);
+    
     digitalWrite(sygZl1_1, HIGH);
     digitalWrite(sygZl1_2, HIGH);
-    digitalWrite(sygZl1_3, HIGH);
     digitalWrite(sygZ, HIGH);
+    
+    digitalWrite(sygZl2_1, HIGH);
+    digitalWrite(sygZl2_2, HIGH);
+    digitalWrite(sygC2, HIGH);
+
+    zmiana = true;
+    delay(1000);
+
+  } else if (digitalRead(guzik) == HIGH && zmiana == true) {
+    
+    digitalWrite(sygZ, LOW);
+    digitalWrite(sygZl1_1, LOW);
+    digitalWrite(sygZl1_2, LOW);
+    digitalWrite(sygZl1_3, HIGH);
+
+    digitalWrite(sygC2, LOW);
+    digitalWrite(sygZl2_1, LOW);
+    digitalWrite(sygZl2_2, LOW);
+    digitalWrite(sygZl2_3, HIGH);
+
+    delay(czasZmiany);
+
+    digitalWrite(sygC, HIGH);
+    digitalWrite(sygZl1_1, HIGH);
+    digitalWrite(sygZl1_2, HIGH);
+    digitalWrite(sygC, HIGH);
+
+    digitalWrite(sygZl2_1, HIGH);
+    digitalWrite(sygZl2_2, HIGH);
+    digitalWrite(sygZ2, HIGH);
+
+    zmiana = false;
   }
 }
 
@@ -71,4 +141,13 @@ void sygnalizacjaKolejowa() {
   digitalWrite(cz2, LOW);
 
   delay(czasMigania);
+}
+
+void testRGB(int r, int g, int b) {
+  // Skrypt do testowania diody RGB, skala 0-255 PWM. Należy używać w void setup()
+  // testRGB WYKLUCZA użycie sygnalizacji drogowej
+
+  analogWrite(9, r);
+  analogWrite(10, g);
+  analogWrite(11, b);
 }
